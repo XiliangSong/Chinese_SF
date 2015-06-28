576a9fcd80ed88502e5c59d1d871fee153fe41f0
boliang <boliangzhang85@gmail.com>
2015-06-28 14:58:50 -0400

    updates

--- setup.py
+++ setup.py
@@ -24,51 +24,6 @@ VERSION = "1.0"
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-path = os.path.dirname(os.path.realpath(__file__))
-
-# install JCC which is required by Pylucene
-jcc_path = os.path.join(path, 'externals/pylucene-4.10.1-1/jcc')
-
-os.chdir(jcc_path)
-
-print('Building JCC...'),
-p = Popen(['python', 'setup.py', 'build'], stdout=PIPE, stderr=PIPE)
-stdout, stderr = p.communicate()
-if stderr:
-    print stderr
-    exit()
-print('Done')
-
-print('Installing JCC...'),
-p = Popen(['python', 'setup.py', 'install'], stdout=PIPE, stderr=PIPE)
-stdout, stderr = p.communicate()
-if stderr:
-    print stderr
-    exit()
-print('Done')
-
-
-# install pylucene
-pylucene_path = os.path.join(path, 'externals/pylucene-4.10.1-1')
-os.chdir(pylucene_path)
-
-print('Building Pylucene...'),
-p = Popen(['make'], stdout=PIPE, stderr=PIPE)
-stdout, stderr = p.communicate()
-if stderr:
-    print stderr
-    exit()
-print('Done')
-
-print('Installing Pylucene...'),
-p = Popen(['make', 'install'], stdout=PIPE, stderr=PIPE)
-stdout, stderr = p.communicate()
-if stderr:
-    print stderr
-    exit()
-print('Done')
-
-
 # python setup to install dependency packages
 setup(
     name=NAME,
